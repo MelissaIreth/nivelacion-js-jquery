58ef4c204be0d27ef02fd3175da2b32197549101
Melissa <melissa.pacheco.martinez@gmail.com>
2017-07-10 19:05:57 -0400

    etapa 9: agregar y quitar clase 'active' de los btns al dar click

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -10,12 +10,17 @@ $(document).ready(function(){
 
 function btns(){
     	$(".js-show-recipe").click(function(){
-			console.log("You can see our recipes")
-			$(".page.recipe").removeClass('make')
+			console.log("You can see our recipes");
+			$(".page.recipe").removeClass('make');
+			$("a").addClass('active');
+			$(".js-show-make").removeClass('active');
+
 	});
     	$(".js-show-make").click(function(){
-    		console.log("And you can also make your own recipes")
-    		$(".page.recipe").addClass('make')
+    		console.log("And you can also make your own recipes");
+    		$(".page.recipe").addClass('make');
+    		$("a").addClass('active');
+			$(".js-show-recipe").removeClass('active');
     });
 
 };

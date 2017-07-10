6fcce468eae4579aa1709bc57f1ef0a798c97c0c
Melissa <melissa.pacheco.martinez@gmail.com>
2017-07-10 18:55:47 -0400

    etapa 8: asigne alert a los botones

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
 	console.log("This is reaaaaady!")
+	btns();
 });
 
     $("#hide").ready(function(){
 		$(".js-menu").hide()
 	});
+
+
+function btns(){
+    	$(".js-show-recipe").click(function(){
+			alert("You can see our recipes")
+			
+	});
+    	$(".js-show-make").click(function(){
+    		alert("And you can also make your own recipes")
+    		
+    });
+
+};
+	
